a0f5c8459c2c08e6a8f2ff5867b7af8189306aa3
PaulzhaoGRL <paulzhao@graniteriverlabs.com>
2025-04-22 11:24:18 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# 安裝 Python 套件
 pip install -r requirements.txt
-playwright install chromium
+
+# 安裝 Playwright
+pip install playwright
+
+# 安裝 Chromium 瀏覽器
+python -m playwright install chromium
